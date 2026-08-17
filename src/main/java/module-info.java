9cca1f285174dda015c9544ddf4f13ca6bb2f463
Kevin Felix <kevin.felix59@uabc.edu.mx>
2026-08-17 16:46:10 -0700

    feat: "Conexion con Java configurada y validada"

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.agendajavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.agendajavafx to javafx.fxml;
-    exports com.example.agendajavafx;
-}
